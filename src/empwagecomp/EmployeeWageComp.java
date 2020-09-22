3f09ad001bc7c5f8edfb105e2dac6d551d9b9033
adityaviren <adityaviren@gmail.com>
2020-09-22 11:15:39 +0530

    UC2 CalculateDaily Employee Wage in child1

--- src/empwagecomp/EmployeeWageComp.java
+++ src/empwagecomp/EmployeeWageComp.java
@@ -5,7 +5,8 @@ public class EmployeeWageComp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Program");
-		
+		isPresent();
+	
 	}
 	private static void isPresent() {
 		double is_present=Math.random();
@@ -13,6 +14,11 @@ public class EmployeeWageComp {
 			System.out.println("Employee is absent");
 		else
 			System.out.println("Employee is present");
+	}
+	private static void calculateDailyWage() {
+		int daily_wage_per_hour=20, no_of_hours=8;
+		long daily_wage=daily_wage_per_hour*no_of_hours;
+		System.out.println("Daily wage of employee = "+daily_wage);
 	}
 
 }

15bfb109a1f5a4157d177f64d719b553b019f1e1
adityaviren <adityaviren@gmail.com>
2020-09-22 11:05:31 +0530

    First commit master

---
+++ src/empwagecomp/EmployeeWageComp.java
@@ -0,0 +1,10 @@
+package empwagecomp;
+
+public class EmployeeWageComp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Employee Wage Computation Program");
+	}
+
+}

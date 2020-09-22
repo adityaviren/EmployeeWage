d07e571dc0db5cc1b1677aee76c3ef7288c5d890
adityaviren <adityaviren@gmail.com>
2020-09-22 11:10:17 +0530

    UC1 in child branch

--- src/empwagecomp/EmployeeWageComp.java
+++ src/empwagecomp/EmployeeWageComp.java
@@ -5,6 +5,14 @@ public class EmployeeWageComp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Program");
+		
+	}
+	private static void isPresent() {
+		double is_present=Math.random();
+		if(is_present<.5)
+			System.out.println("Employee is absent");
+		else
+			System.out.println("Employee is present");
 	}
 
 }

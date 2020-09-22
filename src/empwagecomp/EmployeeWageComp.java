2dc489cae3e7b7e73463f890494b9accd19cef07
adityaviren <adityaviren@gmail.com>
2020-09-22 11:19:36 +0530

    UC3 Part Time added in child1

--- src/empwagecomp/EmployeeWageComp.java
+++ src/empwagecomp/EmployeeWageComp.java
@@ -6,7 +6,8 @@ public class EmployeeWageComp {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Program");
 		isPresent();
-	
+		calculateDailyWage();
+		calculatePartTime();
 	}
 	private static void isPresent() {
 		double is_present=Math.random();
@@ -19,6 +20,11 @@ public class EmployeeWageComp {
 		int daily_wage_per_hour=20, no_of_hours=8;
 		long daily_wage=daily_wage_per_hour*no_of_hours;
 		System.out.println("Daily wage of employee = "+daily_wage);
+	}
+	private static void calculatePartTime() {
+		int part_time_hours=8, part_time_wage_per_hour=20;
+		long part_time_wage=part_time_hours*part_time_wage_per_hour;
+		System.out.println("Part Time wage of employee");
 	}
 
 }

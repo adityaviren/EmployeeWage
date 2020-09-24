1b109290aab4aadc2f1707c6d118264e4e0544ff
adityaviren <adityaviren@gmail.com>
2020-09-24 12:53:59 +0530

    UC3 using switch case

--- src/empwagecomp/EmployeeWageComp.java
+++ src/empwagecomp/EmployeeWageComp.java
@@ -1,14 +1,24 @@
 package empwagecomp;
 
-import java.util.Scanner;
+import java.lang.Math;
 
 public class EmployeeWageComp {
 	private static final int emp_wage_per_hour=20;
 	public static void main(String[] args) {
-		double random_present=Math.random();
-		String message = (random_present < 0.5) ? "Employee is present" : "Employee is absent" ;
-		int emp_hour = (random_present < 0.5) ? 8 : 0;
-		System.out.println(message);
+		int is_present=(int)(Math.random()*30 % 3);
+		int emp_hour=0;
+		switch (is_present) {
+		case 1:
+			emp_hour=8;
+			break;
+		case 2:
+			emp_hour=4;
+			break;
+		default:
+			emp_hour=0;
+			break;
+		}
+		System.out.println("");
 		int emp_daily_wage;
 		emp_daily_wage=emp_hour*emp_wage_per_hour;
 		System.out.println("Employee wage per day is "+emp_daily_wage);

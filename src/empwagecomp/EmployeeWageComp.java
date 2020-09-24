20db12627bd7ad0866d15f2babd853d87d5f2c3f
adityaviren <adityaviren@gmail.com>
2020-09-24 13:00:31 +0530

    UC5 using while loop for 20 days

--- src/empwagecomp/EmployeeWageComp.java
+++ src/empwagecomp/EmployeeWageComp.java
@@ -3,25 +3,31 @@ package empwagecomp;
 import java.lang.Math;
 
 public class EmployeeWageComp {
-	private static final int emp_wage_per_hour=20;
+	private static final int emp_wage_per_hour = 20;
+
 	public static void main(String[] args) {
-		int is_present=(int)(Math.random()*30 % 3);
-		int emp_hour=0;
-		switch (is_present) {
-		case 1:
-			emp_hour=8;
-			break;
-		case 2:
-			emp_hour=4;
-			break;
-		default:
-			emp_hour=0;
-			break;
+		int /*total_emp_hours = 0,*/ total_emp_days = 1,monthly_salary=0;
+		while (/*total_emp_hours <= 100 &&*/ total_emp_days <= 20) {
+			int is_present = (int) (Math.random() * 30 % 3);
+			int emp_hour = 0;
+			switch (is_present) {
+			case 1:
+				emp_hour = 8;
+				break;
+			case 2:
+				emp_hour = 4;
+				break;
+			default:
+				emp_hour = 0;
+				break;
+			}
+			int emp_daily_wage;
+			emp_daily_wage = emp_hour * emp_wage_per_hour;
+			System.out.println("Employee wage per day is " + emp_daily_wage);
+			total_emp_days++;
+			monthly_salary+=emp_daily_wage;
+			/*total_emp_hours+=emp_hour;*/
 		}
-		System.out.println("");
-		int emp_daily_wage;
-		emp_daily_wage=emp_hour*emp_wage_per_hour;
-		System.out.println("Employee wage per day is "+emp_daily_wage);
-		
+		System.out.println("Monthly salary is "+monthly_salary);
 	}
 }
